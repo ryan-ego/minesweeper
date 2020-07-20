2f5dc69540a67114c700a14850bb9f817233ed8d
Ryan Ego <ryan@tnlgk.com>
2020-07-21 10:38:47 +1200

    minesweeper board

--- minesweeper.js
+++ minesweeper.js
@@ -1,7 +1,75 @@
 document.addEventListener('DOMContentLoaded', startGame)
+// Build a board of individual cells grouped into rows and columns
+// Individually, or write a for loop?
+// Build functions to open/flag/mine cells (booleans?)
+// Build a function to randomly place mines
+// Build the ancilary functions (mine count+counter)
 
 // Define your `board` object here!
-// var board = 
+
+// var myObject = {}; // Object literal (preferred way)
+// An object is a collection of properties, and a property is an association between a name (or key) and a value. 
+// A property's value can be a function, in which case the property is known as a method
+// define the board object
+// add property names (keys)to the object called board
+var board = {
+  cells: [{ 
+    row: 0,
+    col: 0,
+    isMine: false,
+    hidden: true,
+  }, 
+  { 
+    row: 0,
+    col: 1,
+    isMine: false,
+    hidden: true,
+  }, 
+  { 
+    row: 0,
+    col: 2,
+    isMine: false,
+    hidden: true,
+  }, 
+  { 
+    row: 1,
+    col: 0,
+    isMine: false,
+    hidden: true,
+  },
+  { 
+    row: 1,
+    col: 1,
+    isMine: false,
+    hidden: true,
+  }, 
+  { 
+    row: 1,
+    col: 2,
+    isMine: false,
+    hidden: true,
+  },
+  { 
+    row: 2,
+    col: 0,
+    isMine: false,
+    hidden: true, 
+  }, 
+  { 
+    row: 2,
+    col: 1,
+    isMine: false,
+    hidden: true,
+  },
+  { 
+    row: 2,
+    col: 2,
+    isMine: false,
+    hidden: true,
+  },
+]
+};
+
 
 function startGame () {
   // Don't remove this function call: it makes the game work!
